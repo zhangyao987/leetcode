ad31c4c43c58e77174c3c00d95011c0e9c1770ca
zy <zhangyao@aixuexi.com>
2021-02-23 22:16:04 +0800

    148. 排序链表

--- src/com/zy/code/Main.java
+++ src/com/zy/code/Main.java
@@ -1340,6 +1340,38 @@ public class Main {
         return result;
     }
 
+    //leetcode148. 排序链表
+    public ListNode sortList(ListNode head) {
+        //归并思想，fast slow找到中间的node 可以先想象成只有两个node的链表，比较left和right值得大小，然后重新构建一个链表
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null){
+            if (left.val < right.val){
+                h.next = left;
+                left = left.next;
+            }else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+
     //leetcode 198. 打家劫舍
     public int rob(int[] nums) {
         if (nums.length == 0){
