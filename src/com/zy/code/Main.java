304010d75fddd3b5f2e61e5e6bb052532cb4838c
zy <zhangyao@aixuexi.com>
2021-03-02 22:06:49 +0800

    200. 岛屿数量

--- src/com/zy/code/Main.java
+++ src/com/zy/code/Main.java
@@ -1223,6 +1223,38 @@ public class Main {
         }
     }
 
+    //leetcode200. 岛屿数量
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i<grid.length;i++){
+            for (int j = 0; j<grid[0].length;j++){
+                if (grid[i][j] == '1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid,int i,int j){
+        if (!numIslandsArea(grid, i, j)) return;
+        if (grid[i][j] != '1') return;
+        grid[i][j] = '2';
+
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+
+    }
+
+    private Boolean numIslandsArea(char[][] grid,int i,int j){
+        return i<grid.length && j<grid[0].length && i>=0 && j>=0;
+    }
+
+
+
     //leetcode239 滑动窗口的最大值 heard
     public int[] maxSlidingWindow(int[] nums, int k) {
 
