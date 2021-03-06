ab3b9244503dfd2de99ff0c8c33f5736234f131d
zy <zhangyao@aixuexi.com>
2021-03-06 23:55:39 +0800

    0-1背包  完全背包问题

--- src/com/zy/code/Main.java
+++ src/com/zy/code/Main.java
@@ -2107,6 +2107,67 @@ public class Main {
 
     }
 
+    /**
+     * 0-1背包问题
+     * @param V	背包容量
+     * @param N	物品种类
+     * @param weight 物品重量
+     * @param value	物品价值
+     * @return
+     */
+    public int ZeroOnePack1(int V,int N,int[] weight,int[] value){
+        int[][] dp = new int[N+1][V+1];
+        for (int i = 1; i<N+1; i++){
+            for (int j = 1; j<V+1; j++){
+                if (weight[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j-weight[i-1]]+value[i-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[N][V];
+    }
+
+    public int ZeroOnePack2(int V,int N,int[] weight,int[] value){
+        int[] dp = new int[V+1];
+        for (int i = 1; i<N; i++){
+            for (int j = V; j>weight[i-1]; j--){
+                dp[j] = Math.max(dp[j-weight[i-1]]+value[i],dp[j]);
+            }
+        }
+        return dp[V];
+    }
+
+    //完全背包
+    //01背包问题是在前一个子问题（i-1种物品）的基础上来解决当前问题（i种物品），
+    // 向i-1种物品时的背包添加第i种物品；而完全背包问题是在解决当前问题（i种物品）向i种物品时的背包添加第i种物品。
+    public String completePack(int V,int N,int[] weight,int[] value){
+        int[][] dp = new int[N+1][V+1];
+        for (int i = 1; i<N+1; i++){
+            for (int j = 1;j<V+1; j++){
+                if (weight[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-weight[i-1]]+value[i-1]);
+                }
+            }
+        }
+        int maxValue = dp[N][V];
+        int j=V;
+        String numStr="";
+        for(int i=N;i>0;i--){
+            //若果dp[i][j]>dp[i-1][j],这说明第i件物品是放入背包的
+            while(dp[i][j]>dp[i-1][j]){
+                numStr = i+" "+numStr;
+                j=j-weight[i-1];
+            }
+            if(j==0)
+                break;
+        }
+        return numStr;
+    }
+
     //凑零钱问题 多少种
     public static int countWays(int[] coins,int n) {
 
