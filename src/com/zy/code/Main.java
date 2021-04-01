d99ccec77ffc8fc4a9cf1b4335fdc4b07e098e06
zy <zhangyao@aixuexi.com>
2021-04-01 23:59:16 +0800

    560. 和为K的子数组(前缀和)

--- src/com/zy/code/Main.java
+++ src/com/zy/code/Main.java
@@ -2065,6 +2065,24 @@ public class Main {
     }
 
 
+    //leetcode560. 和为K的子数组
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+        int sum = 0,res = 0;
+        map.put(0,1);
+        for (int i = 0; i<nums.length;i++){
+            sum += nums[i];
+            if (map.containsKey(sum-k)){
+                res += map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return res;
+    }
+
+
+
     //leetcode643 子数组最大平均数1
     public double findMaxAverage(int[] nums, int k) {
         /*
