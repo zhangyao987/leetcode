a7aeafcb6be524f243e21d2e0107cae25f5e5084
zy <zhangyao@aixuexi.com>
2021-07-04 22:45:24 +0800

    leetcode31. 下一个排列(找较大数较小数)

--- src/com/zy/code/Main.java
+++ src/com/zy/code/Main.java
@@ -942,6 +942,31 @@ public class Main {
         return ~x + 1;
     }
 
+    //leetcode31. 下一个排列
+    public void nextPermutation(int[] nums) {
+        int i = nums.length-2;
+        while (i>=0&& nums[i]>=nums[i+1]){
+            i--;
+        }
+        if (i>=0){
+            int j = nums.length-1;
+            while (j>=0&&nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+
+    }
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
     //leetcode33 搜索旋转数组 时间复杂度logn
     public int search(int[] nums, int target) {
         if(nums == null || nums.length == 0){
