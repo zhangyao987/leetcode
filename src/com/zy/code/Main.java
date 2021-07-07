6ed9d4bc4ebf50f49bcd603d5089e242eb386e5b
zy <zhangyao@aixuexi.com>
2021-07-07 22:08:49 +0800

    leetcode234. 回文链表(中间节点，反转链表)

--- src/com/zy/code/Main.java
+++ src/com/zy/code/Main.java
@@ -1697,6 +1697,56 @@ public class Main {
     }
 
 
+    //leetcode234. 回文链表
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = isPalindrome1(firstHalfEnd.next);
+
+        // 判断是否回文
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // 还原链表并返回结果
+//        firstHalfEnd.next = isPalindrome1(secondHalfStart);
+        return result;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode isPalindrome1(ListNode head){
+        ListNode pp = null,cur = head,newhead = null,next = null;
+        while (cur != null){
+            if (cur.next == null){
+                newhead = cur;
+            }
+            next = cur.next;
+            cur.next = pp;
+            pp = cur;
+            cur = next;
+        }
+        return newhead;
+    }
 
     //leetcode239 滑动窗口的最大值 heard
     public int[] maxSlidingWindow(int[] nums, int k) {
